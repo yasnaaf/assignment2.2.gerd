4732c16054ef64e97d554c75a2ebbaad847b18e3
yasnaaf <152720689+yasnaaf@users.noreply.github.com>
2023-12-23 18:29:52 +0330

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float x ;
+    printf("Enter the number=");
+    scanf("%f",&x);
+
+    int round_off=x;
+
+    printf("round_off=%.0f",x);
+
+    return 0;
+}
